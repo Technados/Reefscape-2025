32d022188d401d4a91fca42ccf0c2359f3110a5c
margot-g73 <margotnmgrace@gmail.com>
2025-02-10 15:45:27 -0500

    Update ClimberSubsystem.java
    setup climber subsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,5 +1,201 @@
 package frc.robot.subsystems;
 
-public class ClimberSubsystem {
-    
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.sim.SparkFlexSim;
+import com.revrobotics.sim.SparkLimitSwitchSim;
+import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants.CoralSubsystemConstants;
+import frc.robot.Constants.CoralSubsystemConstants.ArmSetpoints;
+import frc.robot.Constants.CoralSubsystemConstants.ElevatorSetpoints;
+import frc.robot.Constants.CoralSubsystemConstants.IntakeSetpoints;
+import frc.robot.Constants.SimulationRobotConstants;
+
+public class ClimberSubsystem extends SubsystemBase {
+  /** Subsystem-wide setpoints */
+  public enum Setpoint {
+    kFeederStation,
+    kLevel1,
+    kLevel2,
+    kLevel3,
+    kLevel4;
+  }
+
+  // Initialize arm SPARK. We will use MAXMotion position control for the arm, so we also need to
+  // initialize the closed loop controller and encoder.
+  private SparkMax armMotor =
+      new SparkMax(CoralSubsystemConstants.kArmMotorCanId, MotorType.kBrushless);
+  private SparkClosedLoopController armController = armMotor.getClosedLoopController();
+  private RelativeEncoder armEncoder = armMotor.getEncoder();
+
+  // Initialize intake SPARK. We will use open loop control for this so we don't need a closed loop
+  // controller like above.
+  private SparkMax intakeMotor =
+      new SparkMax(CoralSubsystemConstants.kIntakeMotorCanId, MotorType.kBrushless);
+
+  // Member variables for subsystem state management
+  private boolean wasResetByButton = false;
+  private double armCurrentTarget = ArmSetpoints.kFeederStation;
+
+
+  // Simulation setup and variables
+  private DCMotor armMotorModel = DCMotor.getNEO(1);
+  private SparkMaxSim armMotorSim;
+  private final SingleJointedArmSim m_armSim =
+      new SingleJointedArmSim(
+          armMotorModel,
+          SimulationRobotConstants.kArmReduction,
+          SingleJointedArmSim.estimateMOI(
+              SimulationRobotConstants.kArmLength, SimulationRobotConstants.kArmMass),
+          SimulationRobotConstants.kArmLength,
+          SimulationRobotConstants.kMinAngleRads,
+          SimulationRobotConstants.kMaxAngleRads,
+          true,
+          SimulationRobotConstants.kMinAngleRads,
+          0.0,
+          0.0);
+
+  // Mechanism2d setup for subsystem
+  private final Mechanism2d m_mech2d = new Mechanism2d(50, 50);
+  private final MechanismRoot2d m_mech2dRoot = m_mech2d.getRoot("ElevatorArm Root", 25, 0);
+  private final MechanismLigament2d m_elevatorMech2d =
+      m_mech2dRoot.append(
+          new MechanismLigament2d(
+              "Elevator",
+              SimulationRobotConstants.kMinElevatorHeightMeters
+                  * SimulationRobotConstants.kPixelsPerMeter,
+              90));
+  private final MechanismLigament2d m_armMech2d =
+      m_elevatorMech2d.append(
+          new MechanismLigament2d(
+              "Arm",
+              SimulationRobotConstants.kArmLength * SimulationRobotConstants.kPixelsPerMeter,
+              180 - Units.radiansToDegrees(SimulationRobotConstants.kMinAngleRads) - 90));
+
+  public ClimberSubsystem() {
+    /*
+     * Apply the appropriate configurations to the SPARKs.
+     *
+     * kResetSafeParameters is used to get the SPARK to a known state. This
+     * is useful in case the SPARK is replaced.
+     *
+     * kPersistParameters is used to ensure the configuration is not lost when
+     * the SPARK loses power. This is useful for power cycles that may occur
+     * mid-operation.
+     */
+    armMotor.configure(
+        Configs.CoralSubsystem.armConfig,
+        ResetMode.kResetSafeParameters,
+        PersistMode.kPersistParameters);
+    intakeMotor.configure(
+        Configs.CoralSubsystem.intakeConfig,
+        ResetMode.kResetSafeParameters,
+        PersistMode.kPersistParameters);
+
+    // Display mechanism2d
+    SmartDashboard.putData("Coral Subsystem", m_mech2d);
+
+    // Zero arm and elevator encoders on initialization
+    armEncoder.setPosition(0);
+
+    // Initialize simulation values
+    armMotorSim = new SparkMaxSim(armMotor, armMotorModel);
+  }
+
+  /**
+   * Drive the arm and elevator motors to their respective setpoints. This will use MAXMotion
+   * position control which will allow for a smooth acceleration and deceleration to the mechanisms'
+   * setpoints.
+   */
+  private void moveToSetpoint() {
+    armController.setReference(armCurrentTarget, ControlType.kMAXMotionPositionControl);
+  }
+
+  /** Zero the arm and elevator encoders when the user button is pressed on the roboRIO. */
+  private void zeroOnUserButton() {
+    if (!wasResetByButton && RobotController.getUserButton()) {
+      // Zero the encoders only when button switches from "unpressed" to "pressed" to prevent
+      // constant zeroing while pressed
+      wasResetByButton = true;
+      armEncoder.setPosition(0);
+    } else if (!RobotController.getUserButton()) {
+      wasResetByButton = false;
+    }
+  }
+
+  /** Set the intake motor power in the range of [-1, 1]. */
+  private void setIntakePower(double power) {
+    intakeMotor.set(power);
+  }
+  
+
+  /**
+   * Command to run the intake motor. When the command is interrupted, e.g. the button is released,
+   * the motor will stop.
+   */
+  public Command runIntakeCommand() {
+    return this.startEnd(
+        () -> this.setIntakePower(IntakeSetpoints.kForward), () -> this.setIntakePower(0.0));
+  }
+
+  /**
+   * Command to reverses the intake motor. When the command is interrupted, e.g. the button is
+   * released, the motor will stop.
+   */
+  public Command reverseIntakeCommand() {
+    return this.startEnd(
+        () -> this.setIntakePower(IntakeSetpoints.kReverse), () -> this.setIntakePower(0.0));
+  }
+
+  @Override
+  public void periodic() {
+    moveToSetpoint();
+    zeroOnUserButton();
+
+    // Display subsystem values
+    SmartDashboard.putNumber("Coral/Arm/Target Position", armCurrentTarget);
+    SmartDashboard.putNumber("Coral/Arm/Actual Position", armEncoder.getPosition());
+    SmartDashboard.putNumber("Coral/Intake/Applied Output", intakeMotor.getAppliedOutput());
+
+    // Update mechanism2d
+    m_armMech2d.setAngle(
+        180
+            - ( // mirror the angles so they display in the correct direction
+            Units.radiansToDegrees(SimulationRobotConstants.kMinAngleRads)
+                + Units.rotationsToDegrees(
+                    armEncoder.getPosition() / SimulationRobotConstants.kArmReduction))
+            - 90 // subtract 90 degrees to account for the elevator
+        );
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // Iterate the arm SPARK simulations
+    armMotorSim.iterate(
+        Units.radiansPerSecondToRotationsPerMinute(
+            m_armSim.getVelocityRadPerSec() * SimulationRobotConstants.kArmReduction),
+        RobotController.getBatteryVoltage(),
+        0.02);
+
+    // SimBattery is updated in Robot.java
+  }
 }
